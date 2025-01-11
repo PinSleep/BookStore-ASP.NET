24378ede3cb21ec7dee868f3c67c35349c0eee23
PinSleep <116431528+PinSleep@users.noreply.github.com>
2025-01-11 01:09:16 +0100

    Add files via upload

---
+++ Helpers/SessionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text.Json;
+
+namespace BookStore.Helpers
+{
+    public static class SessionExtensions
+    {
+        public static void Set<T>(this ISession session, string key, T value)
+        {
+            var json = JsonSerializer.Serialize(value);
+            session.SetString(key, json);
+        }
+
+        public static T Get<T>(this ISession session, string key)
+        {
+            var json = session.GetString(key);
+            return json == null ? default(T) : JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
