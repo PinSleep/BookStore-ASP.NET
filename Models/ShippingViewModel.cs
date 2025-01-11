2e326d7aad47c55fced3809d95c35c80e763476f
PinSleep <116431528+PinSleep@users.noreply.github.com>
2025-01-11 01:06:22 +0100

    Add files via upload

---
+++ Models/ShippingViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class ShippingViewModel
+    {
+        [Required(ErrorMessage = "First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Phone prefix is required")]
+        public string PhonePrefix { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Country is required")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Street is required")]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage = "Building number is required")]
+        public string BuildingNumber { get; set; }
+
+        [Required(ErrorMessage = "Postal code is required")]
+        public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "City is required")]
+        public string City { get; set; }
+    }
+}
